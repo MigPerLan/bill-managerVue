b6ef66a097ecdf109c45b01d8498fa01e8f2fafb
Mikeyhey <mikeyhey@hotmail.com>
2019-05-17 16:09:50 -0700

    added some js

--- public/JS/userSetup.js
+++ public/JS/userSetup.js
@@ -1,1 +1,20 @@
-ECHO is on.
+const app = new Vue({
+    el: "#form",
+    methods: {
+        moreBills() {
+            $('#moreBills').append(`
+            <div class="row">
+                <div class="form-row mx-2 mt-2">
+                    <div class="col-4">
+                    <input type="text" class="form-control" placeholder="Bill" id="bills">
+                    </div>
+                    <div class="col-3">
+                    <input type="number" class="form-control" placeholder="$">
+                    </div>
+                </div>
+            </div>
+        `);
+
+        }
+    }
+});
