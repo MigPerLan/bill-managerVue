0b01147ea08fd92c1f3065a1a7634073a4194a25
Mikeyhey <mikeyhey@hotmail.com>
2019-05-17 22:49:14 -0700

    form grabbing all inputs

--- public/JS/userSetup.js
+++ public/JS/userSetup.js
@@ -10,17 +10,17 @@ Vue.component('user-form', {
     </div>
     <div class="form-group">
         <label for="amount">Amount per check:</label>
-        <input type="number" class="form-control" id="amount" aria-describedby="amount" placeholder="$" v-model="amount" number>
+        <input type="number" class="form-control" id="amount" aria-describedby="amount" placeholder="$" v-model.number="amount">
     </div>
     <div class="form-group">
         <label for="savings">Savings:</label>
-        <input type="number" class="form-control" id="savings" aria-describedby="savings" placeholder="$" v-model="savings" number>
+        <input type="number" class="form-control" id="savings" aria-describedby="savings" placeholder="$" v-model.number="savings">
     </div>
     <label for="weekly">Pay Period:</label>
     <div class="row">
         <div class="col">
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="weekly"
+                <input class="form-check-input" type="radio" id="weekly"
                     value="weekly" checked v-model="payPeriod">
                 <label class="form-check-label" for="weekly">
                     Weekly
@@ -29,7 +29,7 @@ Vue.component('user-form', {
         </div>
         <div class="col">
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="biweekly"
+                <input class="form-check-input" type="radio" id="biweekly"
                     value="biweekly" v-model="payPeriod">
                 <label class="form-check-label" for="biweekly">
                     Biweekly
@@ -38,7 +38,7 @@ Vue.component('user-form', {
         </div>
         <div class="col">
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="monthly"
+                <input class="form-check-input" type="radio"  id="monthly"
                     value="monthly" v-model="payPeriod">
                 <label class="form-check-label" for="monthly">
                     Monthly
@@ -46,22 +46,16 @@ Vue.component('user-form', {
             </div>
         </div>
     </div>
-    <label for="bills">Bills:</label>
-    <div class="row">
-        <div class="form-row mx-2">
-            <div class="col-4">
-                <input type="text" class="form-control" placeholder="Bill" id="bills" v-model="billName">
-            </div>
-            <div class="col-3">
-                <input type="number" class="form-control" placeholder="$" v-model="billPrice">
-            </div>
+     <bills-form @setBills="addBills"></bills-form>
+     <div class="row mt-2">
+        <div class= "col=2">
+            <ul>
+            <li v-for="bill in bills">
+            <p> {{ bill.billName }} {{ bill.billCost }} </p>
+            </li>
+            </ul>
         </div>
-        <div class="col">
-            <button type="button" style="border:none; background-color:transparent"
-              v-on:click="moreBills"><i class="fas fa-plus"></i></button>
-        </div>
-    </div>
-    <div id="moreBills"></div>
+     </div>
     <button type="submit"  class="btn btn-primary mt-2">Submit</button>
 </form>
 </div>      
@@ -73,38 +67,64 @@ Vue.component('user-form', {
             amount: null,
             savings: null,
             payPeriod: null,
-            billName: [],
-            billPrice: [],
+            bills: [],
         }
     },
     methods: {
-        moreBills() {
-            $('#moreBills').append(`
-
-            <div class="row">
-                <div class="form-row mx-2 mt-2">
-                    <div class="col-4">
-                    <input type="text" class="form-control" placeholder="Bill" id="bills" v-model="bills">
-                    </div>
-                    <div class="col-3">
-                    <input type="number" class="form-control" placeholder="$">
-                    </div>
-                </div>
-        `);
+        addBills(setBills) {
+            this.bills.push(setBills);
         },
-        onSubmit() {
+        onSubmit(){
             console.log(this.name);
             console.log(this.amount);
             console.log(this.savings);
             console.log(this.payPeriod);
-            console.log(this.billName + " " + this.billPrice);
-
-
+            console.log(this.bills);
         }
-
 
     }
 });
+
+Vue.component('bills-form', {
+    template: `
+<div>
+    <form>
+        <label for="bills">Bills:</label>
+    <div class="row">
+        <div class="form-row mx-2">
+            <div class="col-4">
+                <input type="text" class="form-control" placeholder="Bill" id="bills" v-model="billName" >
+            </div>
+            <div class="col-3">
+                <input type="number" class="form-control" placeholder="$" v-model.number="billCost">
+            </div>
+        </div>
+        <div class="col">
+            <button type="button" style="border:none; background-color:transparent" v-on:click="onClick" ><i class="fas fa-plus"></i></button>
+        </div>
+    </div>
+</form> 
+</div>
+    `,
+    data() {
+        return {
+            billName: null,
+            billCost: null
+        }
+    },
+    methods: {
+
+        onClick() {
+            let billArr = {
+                billName: this.billName,
+                billCost: this.billCost
+            }
+            this.$emit('setBills', billArr);
+            this.billName = null;
+            this.billCost = null;
+        }
+    }
+})
 
 const app = new Vue({
     el: "#app"
